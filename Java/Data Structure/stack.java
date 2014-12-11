1ee9833bafc3fbc8da779c31c91cc8bfa46ab7ff
Linh Bui <linhbui@colgate.edu>
2014-12-10 16:33:47 -0800

    set of stacks (work in progress)

--- Java/Data Structure/stack.java
+++ Java/Data Structure/stack.java
@@ -33,3 +33,70 @@ int peek(int stackNum) {
 
 
 /* implement a stack that also return the minimum element */
+import java.util.List;
+import java.util.ArrayList;
+
+class Stack {
+    Node top;
+    List<Integer> minArray = new ArrayList(); 
+    Node pop() {
+        if (top != null) {
+            Object el = top.data;
+            top = top.next;
+            minArray.pop();
+            return el;
+        }
+        return null;
+    } 
+
+    void push(Object el) {
+        Node nextNode = new Node(el);
+        if ((minArray.size == 0) || (el < minArray.get(minArray.size -1)) {
+            minArray.push(el);
+        } else {
+            minArray.push(minArray.get(minArray.size -1));
+        }
+        nextNode.next = top;
+        top = nextNode;
+    }
+
+    int min() {
+        return minArray.get(minArray.size -1);
+    }
+}
+
+/* SetOfStacks: compose of several stacks, create a new stack once the previous one exceeds capacity */
+class SetOfStacks {
+    ArrayList<Stack> stacks = new ArrayList<Stack>();
+    public int pop() {
+        if (top != null) {
+            Object el = top.data;
+            top = top.next;
+            return el;
+        }
+        return null;
+    } 
+
+    public void push(int el) {
+        Stack lastStack = getLastStack();
+        if (lastStack!= null && !last.isFull()) {
+            lastStack.push(el);
+        } else {
+            Stack stack = new Stack(capacity);
+            stack.push(v);
+            stacks.add(stack);
+        }
+    }
+
+    public void add(Stack stack) {
+        stacks.push(stack);
+    }
+
+    public Stack getLastStack() {
+        return stacks.get(stacks.size -1);
+    }
+
+    public void isFull() {
+        
+    }
+}
